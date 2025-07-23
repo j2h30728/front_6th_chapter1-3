622d29f3b9ad0bba571f3385ea5ff7ae49a1de5a
jihyeon <rachel2148072@gmail.com>
2025-07-24 00:18:59 +0900

    feat: modal provider 리팩토링

--- packages/app/src/components/modal/ModalProvider.tsx
+++ packages/app/src/components/modal/ModalProvider.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, memo, type PropsWithChildren, type ReactNode, useContext, useState } from "react";
+import { createContext, memo, type PropsWithChildren, type ReactNode, useContext, useMemo, useState } from "react";
 import { createPortal } from "react-dom";
 import { Modal } from "./Modal";
+import { useAutoCallback } from "@hanghae-plus/lib";
 
 export const ModalContext = createContext<{
   open: (content: ReactNode) => void;
@@ -16,12 +17,12 @@ export const useModalContext = () => useContext(ModalContext);
 export const ModalProvider = memo(({ children }: PropsWithChildren) => {
   const [content, setContent] = useState<ReactNode>(null);
 
-  const open = (newContent: ReactNode) => setContent(newContent);
+  const open = useAutoCallback((newContent: ReactNode) => setContent(newContent));
+  const close = useAutoCallback(() => setContent(null));
 
-  const close = () => setContent(null);
-
+  const value = useMemo(() => ({ open, close }), [open, close]);
   return (
-    <ModalContext value={{ open, close }}>
+    <ModalContext value={value}>
       {children}
       {content && createPortal(<Modal>{content}</Modal>, document.body)}
     </ModalContext>
